88061b1b9e6add4138b7dc407166f3eab9317bdb
yeseul106 <20191037@sungshin.ac.kr>
2023-07-11 01:11:01 +0900

    [SETTING] common 파일 및 에러 처리 관련 파일 세팅

---
+++ puzzlingServer/src/main/java/com/puzzling/puzzlingServer/common/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.puzzling.puzzlingServer.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+}
---
+++ puzzlingServer/src/main/java/com/puzzling/puzzlingServer/common/exception/BadRequestException.java
@@ -0,0 +1,14 @@
+package com.puzzling.puzzlingServer.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends BaseException {
+
+    public BadRequestException() {
+        super(HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(String responseMessage) {
+        super(HttpStatus.BAD_REQUEST, responseMessage);
+    }
+}
