661406d3f97592f1b4093f773f763f1380644f88
yeseul106 <20191037@sungshin.ac.kr>
2023-07-13 09:37:46 +0900

    #7 [ADD] fail 시, response body에 data:null이지 않도록 data 컬럼값을 없애기

--- puzzlingServer/src/main/java/com/puzzling/puzzlingServer/common/response/ApiResponse.java
+++ puzzlingServer/src/main/java/com/puzzling/puzzlingServer/common/response/ApiResponse.java
@@ -1,10 +1,13 @@
 package com.puzzling.puzzlingServer.common.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     private final int status;
